4fe248c8c5cb97b3aac2e12aebf47d67d82acffc
Oleg Yukhnevich <whyoleg@gmail.com>
2022-07-28 00:08:53 +0300

    working IR injection

--- engine-impl/src/jsMain/kotlin/engine/impl/EngineImpl.kt
+++ engine-impl/src/jsMain/kotlin/engine/impl/EngineImpl.kt
@@ -3,9 +3,11 @@ package engine.impl
 import engine.api.*
 
 @Suppress("DEPRECATION")
-@OptIn(ExperimentalStdlibApi::class)
+@OptIn(ExperimentalStdlibApi::class, ExperimentalJsExport::class)
 @EagerInitialization
-private val initHook = EngineLoader.register(EngineImpl)
+@JsExport
+@Deprecated("", level = DeprecationLevel.HIDDEN)
+public val initHook: dynamic = EngineLoader.register(EngineImpl)
 
 actual object EngineImpl : Engine {
     override val name: String get() = "JS"
